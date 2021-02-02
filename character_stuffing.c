ba3a54a34f314674e797bd108f1678f724958768
shilparani900 <51986971+shilparani900@users.noreply.github.com>
2021-02-02 16:53:56 +0530

    Add files via upload

---
+++ character_stuffing.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+ int main()
+{
+        char a[30], b[50] = " ", t[3], sd, ed, x[3], s[3], d[3], y[3];
+        int i, j, p = 0, q = 0;
+        
+        printf("Enter characters to be stuffed:");
+        scanf("%s", a);
+        printf("\nEnter a character represents star delimiter:");
+        scanf(" %c", &sd);
+        printf("\nEnter a character represents end delimiter:");
+        scanf(" %c", &ed);
+        x[0] = s[0] = s[1] = sd;
+        x[1] = s[2] = '\0';
+        y[0] = d[0] = d[1] = ed;
+        d[2] = y[1] = '\0';
+        strcat(b, x);
+        for(i = 0; i < strlen(a); i++)
+        {
+            t[0] = a[i];
+            t[1] = '\0';
+            if(t[0] == sd)
+                strcat(b, s);
+            else if(t[0] == ed)
+                strcat(b, t);
+            else
+                strcat(b, t);
+        }
+        strcat(b, y);
+        printf("\n After stuffing:%s", b);
+       
+    }
